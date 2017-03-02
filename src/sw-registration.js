d57470a8fb1a4ae8c71e13934364c696dd7e41b8
Quentin Sommer <sommerquentin67@gmail.com>
2017-03-02 15:19:16 +0100

    updated sw

--- src/sw-registration.js
+++ src/sw-registration.js
@@ -22,22 +22,22 @@ if ('serviceWorker' in navigator) {
                 // have been added to the cache.
                 // It's the perfect time to display a "New content is available please refresh."
                 // message in the page's interface.
-                // console.log('New or updated content is available.')
+                console.log('New or updated content is available.')
               } else {
                 // At this point, everything has been precached.
                 // It's the perfect time to display a "Content is cached for offline use." message.
-                // console.log('Content is now available offline!')
+                 console.log('Content is now available offline!')
               }
               break
 
             case 'redundant':
-             // console.error('The installing service worker became redundant.')
+             console.error('The installing service worker became redundant.')
               break
           }
         }
       }
     }).catch(function(e) {
-      // console.error('Error during service worker registration:', e)
+       console.error('Error during service worker registration:', e)
     })
   })
 }
