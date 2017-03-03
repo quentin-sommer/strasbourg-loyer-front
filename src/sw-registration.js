8ab31f23ecfbbd38511b71097bec0f3f72d72910
Quentin Sommer <sommerquentin67@gmail.com>
2017-03-03 10:24:09 +0100

    removed sw debug

--- src/sw-registration.js
+++ src/sw-registration.js
@@ -10,7 +10,7 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('service-worker.js').then(function(reg) {
       // updatefound is fired if service-worker.js changes.
       reg.onupdatefound = function() {
-        console.log('update found')
+        // console.log('update found')
         // The updatefound event implies that reg.installing is set see
         // https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#service-worker-container-updatefound-event
         const installingWorker = reg.installing
@@ -23,11 +23,11 @@ if ('serviceWorker' in navigator) {
                 // have been added to the cache.
                 // It's the perfect time to display a "New content is available please refresh."
                 // message in the page's interface.
-                console.log('New or updated content is available.')
+                // console.log('New or updated content is available.')
               } else {
                 // At this point, everything has been precached.
                 // It's the perfect time to display a "Content is cached for offline use." message.
-                 console.log('Content is now available offline!')
+                 // console.log('Content is now available offline!')
               }
               break
 
