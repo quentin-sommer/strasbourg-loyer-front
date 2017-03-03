f8eaeea6351b304fa803c0efb165ead6b8f0c383
Quentin Sommer <sommerquentin67@gmail.com>
2017-03-03 10:24:17 +0100

    coding style sw

--- src/sw-registration.js
+++ src/sw-registration.js
@@ -2,20 +2,20 @@ if ('serviceWorker' in navigator) {
   // Delay registration until after the page has loaded, to ensure that our
   // precaching requests don't degrade the first visit experience.
   // See https://developers.google.com/web/fundamentals/instant-and-offline/service-worker/registration
-  window.addEventListener('load', function() {
+  window.addEventListener('load', function () {
     // Your service-worker.js *must* be located at the top-level directory relative to your site.
     // It won't be able to control pages unless it's located at the same level or higher than them.
     // *Don't* register service worker file in, e.g., a scripts/ sub-directory!
     // See https://github.com/slightlyoff/ServiceWorker/issues/468
-    navigator.serviceWorker.register('service-worker.js').then(function(reg) {
+    navigator.serviceWorker.register('service-worker.js').then(function (reg) {
       // updatefound is fired if service-worker.js changes.
-      reg.onupdatefound = function() {
+      reg.onupdatefound = function () {
         // console.log('update found')
         // The updatefound event implies that reg.installing is set see
         // https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#service-worker-container-updatefound-event
         const installingWorker = reg.installing
 
-        installingWorker.onstatechange = function() {
+        installingWorker.onstatechange = function () {
           switch (installingWorker.state) {
             case 'installed':
               if (navigator.serviceWorker.controller) {
@@ -27,18 +27,18 @@ if ('serviceWorker' in navigator) {
               } else {
                 // At this point, everything has been precached.
                 // It's the perfect time to display a "Content is cached for offline use." message.
-                 // console.log('Content is now available offline!')
+                // console.log('Content is now available offline!')
               }
               break
 
             case 'redundant':
-             console.error('The installing service worker became redundant.')
+              console.error('The installing service worker became redundant.')
               break
           }
         }
       }
-    }).catch(function(e) {
-       console.error('Error during service worker registration:', e)
+    }).catch(function (e) {
+      console.error('Error during service worker registration:', e)
     })
   })
 }
